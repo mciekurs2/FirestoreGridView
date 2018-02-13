dcfffcb94cf119c3093e89a6c0a73a72de206326
mciekurs <mciekurs@outlook.com>
2018-02-13 02:42:48 +0200

    ProgressBar and AppBar modif

--- app/src/main/java/com/example/mrticiekurs/firestoregridview/FullscreenActivity.java
+++ app/src/main/java/com/example/mrticiekurs/firestoregridview/FullscreenActivity.java
@@ -1,18 +1,29 @@
 package com.example.mrticiekurs.firestoregridview;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 
 public class FullscreenActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private ProgressBar progressBar;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +33,26 @@ public class FullscreenActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         String url = extras.getString("url");
         imageView = findViewById(R.id.fullImageView);
+        progressBar = findViewById(R.id.progFull);
+        toolbar = findViewById(R.id.toolbarTrans);
+        setSupportActionBar(toolbar);
 
-        Glide.with(getApplicationContext()).load(url).transition(DrawableTransitionOptions.withCrossFade()).apply(RequestOptions.centerCropTransform().diskCacheStrategy(DiskCacheStrategy.DATA)).into(imageView);
+        Glide.with(getApplicationContext()).load(url).
+                transition(DrawableTransitionOptions.withCrossFade()).
+                apply(RequestOptions.centerCropTransform().
+                        diskCacheStrategy(DiskCacheStrategy.DATA)).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                progressBar.setVisibility(View.GONE);
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                progressBar.setVisibility(View.GONE);
+                return false;
+            }
+        }).into(imageView);
 
     }
 
